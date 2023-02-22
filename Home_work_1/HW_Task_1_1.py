0ce3e9ea22bc8646ed3ff598e31fefebfabbe4bc
Dmitriy <martin-hunt@list.ru>
2023-02-22 13:49:22 +0300

    HW_Complete(+2)

--- Home_work_1/HW_Task_1_1.py
+++ Home_work_1/HW_Task_1_1.py
@@ -14,12 +14,21 @@ clear()
 # else:
 #     print("ERROR! Enter a three-digit number!")
 
-# Вариант 2 (Для любого количества цифр)
+# ---------------Вариант 2 (Для любого количества цифр)
 
-User_number = int(input("Введите число: "))
+# User_number = int(input("Введите число: "))
+# Resultvalue = 0
+# while User_number > 0:
+#     ostatok = User_number % 10
+#     Resultvalue += ostatok
+#     User_number//=10
+# print(f'Сумма цифр данного числа = {Resultvalue}')
+
+# Список -----------------------
+User_number = input("Введите число: ")
 Resultvalue = 0
-while User_number > 0:
-    ostatok = User_number % 10
-    Resultvalue += ostatok
-    User_number//=10
-print(f'Сумма цифр данного числа = {Resultvalue}')
+if len(User_number) != 3:
+    print("ERROR! Enter a three-digit number!")
+else:
+    Resultvalue = int (User_number[0])+ int (User_number[1])+int (User_number[2])
+    print(f'Сумма цифр данного числа = {Resultvalue}')
